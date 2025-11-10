d6be881a97c30320200b0490723e435338515391
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-11-10 20:38:29 +0200

    reaper: Update big cores for Mi439

--- reaper.cpp
+++ reaper.cpp
@@ -119,7 +119,7 @@ static void* reaper_main(void* param) {
         }
     };
  
-    parseCpusets(android::base::GetProperty("persist.sys.axion_cpu_big", "4,5,6,7"), big_cores);
+    parseCpusets(android::base::GetProperty("persist.sys.axion_cpu_big", "0,1,2,3"), big_cores);
  
     for (int core : big_cores) {
         CPU_SET(core, &cpuset);
